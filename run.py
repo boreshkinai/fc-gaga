4367b64b99620769efd6d600853974f306ac09df
Boris Oreshkin <boris.oreshkin@gmail.com>
2022-01-01 18:24:06 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
+# import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+print("Tensorflow version:", tf.__version__)
+
+
+from dataset import Dataset
+from model import Trainer, Parameters
+from model import hyperparams_defaults as hyperparams_dict
+
+
+
 
