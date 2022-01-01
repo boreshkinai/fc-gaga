2c17904c77285e35fc4d37c691edbe19b8845df3
Boris Oreshkin <boris.oreshkin@gmail.com>
2022-01-01 18:33:44 -0500

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ from dataset import Dataset
 from model import Trainer, Parameters
 from model import hyperparams_defaults as hyperparams_dict
 
+print("*********************************")
+print("Default FC-GAGA parameters:")
+print(hyperparams_dic)
+print("*********************************")
 
+hyperparams_dict["dataset"] = 'metr-la'
+hyperparams_dict["horizon"] = 12
+hyperparams_dict["history_length"] = 12
 
+dataset = Dataset(name=hyperparams_dict["dataset"], 
+                  horizon=hyperparams_dict["horizon"], 
+                  history_length=hyperparams_dict["history_length"],
+                  path=DATADIR)
 
+hyperparams_dict["num_nodes"] = dataset.num_nodes
+hyperparams = Parameters(**hyperparams_dict)
